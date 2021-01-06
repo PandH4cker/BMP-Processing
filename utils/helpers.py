e05a81f97632600cdb440f074dd5668f2c8db0b4
MrrRaph <dray.raph@gmail.com>
2021-01-06 21:14:48 +0100

    Added Rotated image for 90, 180 and 270 degree

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import os
+
 def byteToHex(b):
     """Convert byte to Hex in readable format
     
@@ -36,3 +38,60 @@ def npToArray(ndarray):
     for i in range(len(ndarray)):
         octets.append(int(ndarray[i]))
     return bytearray(octets)
+
+def saveBMP(bmp, imageData, outputFile):
+    f = open(outputFile, 'wb')
+
+    signature = bytearray(bmp.signature)
+    f.write(signature)
+
+    totalSize = bytearray(bmp.totalSize)
+    f.write(totalSize)
+
+    appImage = bytearray(bmp.appImage)
+    f.write(appImage)
+
+    startingOffset = bytearray(bmp.startingOffset)
+    f.write(startingOffset)
+
+    headerSize = bytearray(bmp.headerSize)
+    f.write(headerSize)
+
+    width = bytearray(bmp.width)
+    f.write(width)
+
+    height = bytearray(bmp.height)
+    f.write(height)
+
+    planes = bytearray(bmp.planes)
+    f.write(planes)
+
+    bpp = bytearray(bmp.bpp)
+    f.write(bpp)
+
+    compressionType = bytearray(bmp.compressionType)
+    f.write(compressionType)
+
+    imageSize = bytearray(bmp.imageSize)
+    f.write(imageSize)
+
+    hRes = bytearray(bmp.hRes)
+    f.write(hRes)
+
+    vRes = bytearray(bmp.vRes)
+    f.write(vRes)
+
+    numberOfColors = bytearray(bmp.numberOfColors)
+    f.write(numberOfColors)
+
+    numberOfImportantColors = bytearray(bmp.numberOfImportantColors)
+    f.write(numberOfImportantColors)
+
+    height = int(readLittleEndian(bmp.height))
+    width = int(readLittleEndian(bmp.width))
+
+    for i in range(height):
+        for j in range(width):
+            f.write(npToArray(imageData[i][j]))
+
+    f.close()
